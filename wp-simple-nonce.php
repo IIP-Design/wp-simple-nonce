d969c1dc4a175de4212629994203270cfeaaa6c3
Nathan Kleekamp <nkleekamp@gmail.com>
2016-11-18 08:57:04 -0500

    version bump

--- wp-simple-nonce.php
+++ wp-simple-nonce.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: wp-simple-nonce
-Version: 1.2
+Version: 1.3
 Description: A very simple NONCE system for WordPress Developers
 Author: Cal Evans <cal@calevans.com>
 Author URI: http://blog.calevans.com
